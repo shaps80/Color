29bbec8a7939ffcca988209aa56d42203a5f15d5
Shaps Benkau <shapsuk@me.com>
2018-07-10 16:14:30 +0100

    Initial Commit

---
+++ Color+Demo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Color+Demo
+//
+//  Created by Shaps Benkau on 10/07/2018.
+//  Copyright © 2018 152 Percent Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
